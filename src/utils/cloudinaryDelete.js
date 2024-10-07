02ee8469d35ce81a8999ef263223df2f72b2efd2
ayush kumar <socialacc211@gmail.com>
2024-10-08 01:13:52 +0530

    cloudinaryDelete fixes

--- src/utils/cloudinaryDelete.js
+++ src/utils/cloudinaryDelete.js
@@ -12,7 +12,7 @@ cloudinary.config({
 });
 
 export const cloudinaryDelete = async (url)=> {
-  console.log(url)
+  // console.log(url)
   console.log("inside delete")
   if(!url)
     throw new ApiError(
@@ -21,7 +21,7 @@ export const cloudinaryDelete = async (url)=> {
     )
   let arr = url.split('/')
   // let public_id;
-  // if( arr[7] == '.png') {
+  // if( arr[7] == '.png') 
   //   public_id = arr[7].replace(".png","")
   // } else if ( arr[7] == '.jpg') {
   //   public_id =arr[7].replace(".jpg","")
@@ -30,8 +30,28 @@ export const cloudinaryDelete = async (url)=> {
   // }
   
   let public_id = arr[arr.length -1 ].split(".")[0]
+  console.log(public_id)
+  let extension = arr[arr.length -1 ].split(".")[1]
+  console.log(extension)  
+  if( extension == 'mp4') {
+    const response = await cloudinary.uploader.destroy(public_id, {resource_type: 'video'}, (error, result) => {
+      if (error) {
+        console.error('Error deleting video:', error);
+      } else {
+        console.log('Video deleted successfully:', result);
+      }
+    });
+    return response
+  } else {
+    const response = await cloudinary.uploader.destroy(public_id, (error, result) => {
+      if (error) {
+        console.error('Error deleting video:', error);
+      } else {
+        console.log('Video deleted successfully:', result);
+      }
+    });
+    return response
+  }
 
-
-  const response = await cloudinary.uploader.destroy(public_id)
-  return response
+  
 }
